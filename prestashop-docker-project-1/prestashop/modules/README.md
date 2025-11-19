db7556e2c5291b10a33a3d7b23f546678752ffde
Mikołaj <miki.szagi@gmail.com>
2025-11-19 20:30:29 +0100

    dodany i zmountowany folder "prestashop"

---
+++ prestashop-docker-project-1/prestashop/modules/README.md
@@ -0,0 +1,25 @@
+# PrestaShop Modules Documentation
+
+This directory contains the modules for PrestaShop, which extend the functionality of the platform. Below are some key points regarding the modules:
+
+## Installation
+
+To install a module, follow these steps:
+
+1. Download the module files.
+2. Place the module folder in the `prestashop/modules` directory.
+3. Log in to the PrestaShop back office.
+4. Navigate to the "Modules" section.
+5. Find the module in the list and click "Install".
+
+## Usage
+
+Once installed, you can configure the module settings from the PrestaShop back office. Each module may have its own specific configuration options.
+
+## Available Modules
+
+- **Module Name 1**: Brief description of what this module does.
+- **Module Name 2**: Brief description of what this module does.
+- **Module Name 3**: Brief description of what this module does.
+
+For more detailed information about each module, please refer to the documentation provided with the module files.
---
+++ prestashop-docker-project-1/prestashop/modules/ps_apiresources/src/ApiPlatform/Resources/Hook.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License version 3.0
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/AFL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License version 3.0
+ */
+
+declare(strict_types=1);
+
+namespace PrestaShop\Module\APIResources\ApiPlatform\Resources;
+
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+use PrestaShop\PrestaShop\Core\Domain\Hook\Command\UpdateHookStatusCommand;
+use PrestaShop\PrestaShop\Core\Domain\Hook\Exception\HookNotFoundException;
+use PrestaShop\PrestaShop\Core\Domain\Hook\Query\GetHook;
+use PrestaShopBundle\ApiPlatform\Metadata\CQRSGet;
+use PrestaShopBundle\ApiPlatform\Metadata\CQRSUpdate;
+use PrestaShopBundle\ApiPlatform\Metadata\PaginatedList;
+use PrestaShopBundle\ApiPlatform\Provider\QueryListProvider;
+
+#[ApiResource(
+    operations: [
+        new CQRSUpdate(
+            uriTemplate: '/hook-status/{hookId}',
+            CQRSCommand: UpdateHookStatusCommand::class,
+            CQRSQuery: GetHook::class,
+            scopes: ['hook_write'],
+            CQRSQueryMapping: self::MAPPING,
+            CQRSCommandMapping: self::MAPPING,
+        ),
+        new CQRSGet(
+            uriTemplate: '/hook/{hookId}',
+            requirements: ['hookId' => '\d+'],
+            exceptionToStatus: [HookNotFoundException::class => 404],
+            CQRSQuery: GetHook::class,
+            scopes: ['hook_read'],
+            CQRSQueryMapping: self::MAPPING,
+        ),
+        new PaginatedList(
+            uriTemplate: '/hooks',
+            provider: QueryListProvider::class,
+            scopes: ['hook_read'],
+            ApiResourceMapping: ['[id_hook]' => '[hookId]'],
+            gridDataFactory: 'prestashop.core.grid.data_factory.hook',
+            filtersMapping: [
+                '[hookId]' => '[id_hook]',
+            ],
+        ),
+    ],
+)]
+class Hook
+{
+    #[ApiProperty(identifier: true)]
+    public int $hookId;
+
+    public bool $active;
+
+    public string $name;
+
+    public string $title;
+
+    public string $description;
+
+    protected const MAPPING = [
+        // Transforms the url hookId parameter into the $id parameter for GetHook
+        '[hookId]' => '[id]',
+        // Transforms the query result Hook::getId into the Api resource hookId
+        '[id]' => '[hookId]',
+    ];
+}
